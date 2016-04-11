ce311db9c944844f43bb9c62f329ef343ca25189
Sqrt of Saturn <sqrt@octoblu.com>
2016-04-11 07:19:14 -0700

    the basics

---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/codegangsta/cli"
+	"github.com/coreos/go-semver/semver"
+	De "github.com/tj/go-debug"
+)
+
+var debug = De.Debug("meshblu-connector-ignition:main")
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "meshblu-connector-ignition"
+	app.Version = version()
+	app.Action = run
+	app.Flags = []cli.Flag{}
+	app.Run(os.Args)
+}
+
+func run(context *cli.Context) {
+	sigTerm := make(chan os.Signal)
+	signal.Notify(sigTerm, syscall.SIGTERM)
+
+	sigTermReceived := false
+
+	go func() {
+		<-sigTerm
+		fmt.Println("SIGTERM received, waiting to exit")
+		sigTermReceived = true
+	}()
+
+	for {
+		if sigTermReceived {
+			fmt.Println("I'll be back.")
+			os.Exit(0)
+		}
+
+		debug("meshblu-connector-ignition.loop")
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func version() string {
+	version, err := semver.NewVersion(VERSION)
+	if err != nil {
+		errorMessage := fmt.Sprintf("Error with version number: %v", VERSION)
+		log.Panicln(errorMessage, err.Error())
+	}
+	return version.String()
+}
